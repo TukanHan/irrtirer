bd17f19bf36a0be519f969f59a4c1bb2fa9ee0bb
DESKTOP-8USVFTG\Admin <patrykzbiciak@onet.pl>
2025-12-26 20:59:00 +0100

    fix: expandable panel

--- frontend/projects/irrtirer/src/app/shared/expandable-panel/expandable-panel.component.ts
+++ frontend/projects/irrtirer/src/app/shared/expandable-panel/expandable-panel.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, ElementRef, inject, input, OnDestroy, OnInit, signal } from '@angular/core';
 
 @Component({
     selector: 'app-expandable-panel',
@@ -7,18 +7,36 @@ import { ChangeDetectionStrategy, Component, ElementRef, inject, input } from '@
     changeDetection: ChangeDetectionStrategy.OnPush,
     host: {
         '[class.open]': 'isOpen()',
-        '[style.--expand-duration]': 'getExpandDuration() + "ms"',
-        '[style.--panel-height]': 'elementRef.nativeElement.scrollHeight + "px"',
+        '[style.--expand-duration]': 'expandDuration() + "ms"',
+        '[style.--panel-height]': 'panelHeight() + "px"',
     },
 })
-export class ExpandablePanelComponent {
+export class ExpandablePanelComponent implements OnInit, OnDestroy {
     public readonly isOpen = input.required<boolean>();
+
+    protected readonly panelHeight = signal<number>(0);
+
+    private readonly observer: MutationObserver = new MutationObserver(() => {
+        this.panelHeight.set(this.elementRef.nativeElement.scrollHeight);
+    });
 
     protected readonly elementRef = inject<ElementRef<HTMLElement>>(ElementRef);
 
-    protected getExpandDuration(): number {
-        const height = this.elementRef.nativeElement.scrollHeight;
+    protected readonly expandDuration = computed(() => {
+        const height = this.panelHeight();
         const time = 150 + Math.sqrt(height) * 5;
         return this.isOpen() ? time : time * 0.66;
+    });
+
+    public ngOnInit(): void {
+        this.observer.observe(this.elementRef.nativeElement, {
+            attributeFilter: ['scrollHeight'],
+            subtree: true,
+            childList: true,
+        });
+    }
+
+    public ngOnDestroy(): void {
+        this.observer.disconnect();
     }
 }
