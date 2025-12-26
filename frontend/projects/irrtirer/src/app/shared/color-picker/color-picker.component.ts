d16eaf82ee5408914b0e3ec8b96179e4689bf6ed
DESKTOP-8USVFTG\Admin <patrykzbiciak@onet.pl>
2025-12-26 21:50:01 +0100

    feat: FormValueControl in color picker

--- frontend/projects/irrtirer/src/app/shared/color-picker/color-picker.component.ts
+++ frontend/projects/irrtirer/src/app/shared/color-picker/color-picker.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component, forwardRef, input, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, forwardRef, input, model, signal } from '@angular/core';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { ColorSelectionPanelComponent } from './color-selection-panel/color-selection-panel.component';
 import { ColorInstance } from 'color';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { FormValueControl } from '@angular/forms/signals';
 
 @Component({
     selector: 'app-color-picker',
@@ -18,32 +19,35 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
     styleUrl: './color-picker.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ColorPickerComponent implements ControlValueAccessor {
+export class ColorPickerComponent implements ControlValueAccessor, FormValueControl<string>  {
     public readonly label = input.required<string>();
 
-    protected readonly hexColor = signal<string>('#000000');
+    public readonly value = model<string>('#000000');
+
+    public readonly disabled = model<boolean>(false);
 
     protected readonly isOpen = signal<boolean>(false);
-
-    protected readonly isDisabled = signal<boolean>(false);
 
     private onChange: (color: string) => void = () => {};
 
     private onTouched: () => void = () => {};
 
+    protected readonly valueChangedEffect = effect(() => {
+        this.onChange(this.value());
+    });
+
     protected toggleColorPanel(): void {
-        if(!this.isDisabled()) {
+        if(!this.disabled()) {
             this.isOpen.update((value) => !value);
         }
     }
 
     protected onColorChanged(color: ColorInstance): void {
-        this.hexColor.set(color.hex());
-        this.onChange(color.hex());
+        this.value.set(color.hex());
     }
 
     public writeValue(value: string): void {
-        this.hexColor.set(value);
+        this.value.set(value);
     }
 
     public registerOnChange(fn: (color: string) => void): void {
@@ -55,6 +59,6 @@ export class ColorPickerComponent implements ControlValueAccessor {
     }
 
     public setDisabledState(isDisabled: boolean): void {
-        this.isDisabled.set(isDisabled);
+        this.disabled.set(isDisabled);
     }
 }
